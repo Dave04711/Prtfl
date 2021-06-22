027a34b524c38e4ab147e79ce9181d8cdcb092e1
Dave04711 <41294600+Dave04711@users.noreply.github.com>
2021-06-22 22:21:34 +0200

    Add files via upload

---
+++ Tooltips System [ Our ( Last ) Town ]/Tooltip.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+[ExecuteInEditMode()]
+public class Tooltip : MonoBehaviour
+{
+    public TextMeshProUGUI headerField;
+    public TextMeshProUGUI contentField;
+    public LayoutElement layoutElement;
+    public int characterWrapLimit;
+
+    public RectTransform rectTransform;
+
+    private void Awake()
+    {
+        rectTransform = GetComponent<RectTransform>();
+    }
+
+    public void SetText(string content, string header = "")
+    {
+        if (string.IsNullOrEmpty(header)) { headerField.gameObject.SetActive(false); }
+        else
+        {
+            headerField.gameObject.SetActive(true);
+            headerField.text = header;
+        }
+        contentField.text = content;
+        SetWidth();
+    }
+
+    void SetWidth()
+    {
+        int headerLength = headerField.text.Length;
+        int contentLength = contentField.text.Length;
+
+        layoutElement.enabled = (headerLength > characterWrapLimit || contentLength > characterWrapLimit) ? true : false;
+    }
+
+    private void Update()
+    {
+        if (Application.isEditor)
+        {
+            SetWidth();
+        }
+
+        Vector2 mousePosition = Input.mousePosition;
+        float pivotX = mousePosition.x / Screen.width;
+        float pivotY = mousePosition.y / Screen.height;
+        rectTransform.pivot = new Vector2(pivotX, pivotY);
+        transform.position = mousePosition;
+    }
+}
---
+++ Tooltips System [ Our ( Last ) Town ]/TooltipTrigger.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class TooltipTrigger : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    public string header;
+    [TextArea(3,10)]
+    public string content;
+
+    IEnumerator Show()
+    {
+        yield return new WaitForSecondsRealtime(.5f);
+        TooltipSystem.Show(content, header);
+    }
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        StopAllCoroutines();
+        StartCoroutine(Show());
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        StopAllCoroutines();
+        TooltipSystem.Hide();
+    }
+}
